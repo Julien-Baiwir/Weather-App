fc26780e5735cf1ef0e9ff2ec443c25a3fd49c2d
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-15 16:23:59 +0100

    fonctions api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,79 @@
-async function getCityCoordinates(city) {
+async function getCityDataAndCoordonates(city) {
     const geocodingApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c29a355949954abe9d576d08246566b1&units=metric`;
+
     try {
         const response = await fetch(geocodingApi);
         const data = await response.json();
-        console.log(data);
-        return data;  
-       
+        
+        const weatherDescription = data.weather[0].description;
+        const mainTemperature = data.main.temp;
+        const minTemperature = data.main.temp_min;
+        const maxTemperature = data.main.temp_max;
+
+        const latitude = data.coord.lat;
+        const longitude = data.coord.lon;
+
+        return {
+            weatherDescription,
+            mainTemperature,
+            minTemperature,
+            maxTemperature,
+            coordinates: { latitude, longitude },
+        };
+
     } catch (error) {
         console.error('Error requesting API', error);
         throw error;
     }
 }
-getCityCoordinates('London');
 
-async function getWeatherforecast(latitude, longitude) {
-    const weatherForecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=c29a355949954abe9d576d08246566b1&units=metric`;
+
+async function getCityNameandForecast(city) {
     try {
+        const { coordinates } = await getCityDataAndCoordonates(city);
+
+        const weatherForecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=c29a355949954abe9d576d08246566b1&units=metric`;
+
         const response = await fetch(weatherForecastApi);
         const data = await response.json();
-        console.log(data);
-        return data;
+
+        const cityName = data.city.name;
+        const forecastList = data.list;
+
+        return{
+            cityName,
+            forecastList,
+        }
 
     } catch (error) {
         console.error('Error requesting API', error);
         throw error;
     }
 }
-getWeatherforecast(51.5085,
-    -0.1257);
+
+
+async function handleSearchButtonClick() {
+   
+    const userInput = document.getElementById('UserInput').value;
+
+    try {
+        
+        const cityData = await getCityDataAndCoordonates(userInput);
+        const forecastData = await getCityNameandForecast(userInput);
+
+        // Do something with the data (e.g., update the DOM)
+        console.log(cityData);
+        console.log(forecastData);
+    } catch (error) {
+        console.error('An error occurred:', error);
+       
+    }
+}
+
+document.getElementById('SearchButton').addEventListener('click', handleSearchButtonClick);
+
+
+
+
  
   
