d3f70ba54226e353ef9767e2fa5358fbc4025d10
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-16 10:14:40 +0100

    autocomplete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
+import { format } from 'https://cdn.jsdelivr.net/npm/date-fns@2.24.0/esm/index.js';
+
+
+async function Citiesearch(citiyoptions) {
+    const datalistVille = document.getElementById('searchoptions');
+    try {
+
+        const response = await fetch('https://nominatim.openstreetmap.org/search?city=${saisie}&format=json');
+        const data = await response.json();
+        data.innerHTML = '';
+
+        data.forEach(city => {
+            const option = document.createElement('option');
+            option.value = city.display_name;
+            datalistVille.appendChild(option);
+
+        });
+    } catch (error) {
+        console.error('Erreur lors du chargement des villes :', error);
+    }
+}
+
+
+
 async function getCityWeather(city) {
     const geocodingApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c29a355949954abe9d576d08246566b1&units=metric`;
 
     try {
         const response = await fetch(geocodingApi);
         const data = await response.json();
-        
+        console.log(data);
         const weatherDescription = data.weather[0].description;
         const mainTemperature = data.main.temp;
         const minTemperature = data.main.temp_min;
         const maxTemperature = data.main.temp_max;
-
+        const timestamp = data.dt;
         const latitude = data.coord.lat;
         const longitude = data.coord.lon;
 
@@ -18,6 +42,7 @@ async function getCityWeather(city) {
             mainTemperature,
             minTemperature,
             maxTemperature,
+            timestamp,
             coordinates: { latitude, longitude },
         };
 
@@ -36,11 +61,11 @@ async function getCityForecast(city) {
 
         const response = await fetch(ForecastApi);
         const data = await response.json();
-
+        // console.log(data);
         const cityName = data.city.name;
         const forecastList = data.list;
 
-        return{
+        return {
             cityName,
             forecastList,
         }
@@ -50,11 +75,21 @@ async function getCityForecast(city) {
         throw error;
     }
 }
+// ------CURRENT TIME---------
+// addHours
+
+function formatTimestamp(timestamp) {
+    const timestampInMilliseconds = timestamp * 1000;
+    const myDate = new Date(timestampInMilliseconds);
+    const dateFormat = 'yyyy-MM-dd HH:mm:ss';
+    return format(myDate, dateFormat);
+}
+
 
 // ---------- DOM --------------
 function addDomElements(CityWeather, CityForecast) {
-    const { weatherDescription, mainTemperature, minTemperature, maxTemperature } = CityWeather;
-    const {cityName} = CityForecast;
+    const { weatherDescription, mainTemperature, timestamp, minTemperature, maxTemperature } = CityWeather;
+    const { cityName } = CityForecast;
     const MainTemperature = Math.round(mainTemperature);
     const MinTemperature = Math.round(minTemperature);
     const MaxTemperature = Math.round(maxTemperature);
@@ -65,44 +100,48 @@ function addDomElements(CityWeather, CityForecast) {
     resultContainer.querySelector('.app__result-container__weather-desc').textContent = `Weather: ${weatherDescription}`;
     resultContainer.querySelector('.app__result-container__degre').textContent = `${MainTemperature}°`;
 
- 
+
     resultContainer.querySelector('.app__result-container__temp-wrap__min__title').textContent = 'Min';
     resultContainer.querySelector('.app__result-container__temp-wrap__min__temp').textContent = `${MinTemperature}°`;
 
     resultContainer.querySelector('.app__result-container__temp-wrap__max__title').textContent = 'Max';
     resultContainer.querySelector('.app__result-container__temp-wrap__max__temp').textContent = `${MaxTemperature}°`;
 
-   
-    const currentDate = new Date(); 
-    resultContainer.querySelector('.app__result-container__date').textContent = `${currentDate.toDateString()}`;
-  
+    const formattedDate = formatTimestamp(timestamp);
+    resultContainer.querySelector('.app__result-container__date').textContent = formattedDate;
+
 }
 
 
 
 // ----------MAIN CLICK --------------
 async function handleNewUserInput() {
-   
-    const userInput = document.getElementById ('UserInput').value;
+
+    const userInput = document.getElementsByClassName('UserInput').value;
 
     try {
-        
+
         const CityWeather = await getCityWeather(userInput);
         const CityForecast = await getCityForecast(userInput);
-        
+
         console.log(CityWeather);
-        console.log(CityForecast);
+        // console.log(CityForecast);
         addDomElements(CityWeather, CityForecast);
+
+        // const timestamp = CityWeather.timestamp;
+        // const formattedDate = formatTimestamp(timestamp);
+        // console.log(formattedDate);
 
         return { CityWeather, CityForecast };
 
     } catch (error) {
         console.error('An error occurred:', error);
-       
+
     }
 }
 document.getElementById('SearchButton').addEventListener('click', handleNewUserInput);
- 
 
+
+// --------------
 
 
