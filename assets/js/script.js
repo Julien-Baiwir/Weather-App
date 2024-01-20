2a82c1e0a5b9dbd89f8d562502e891dd8da5592b
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-20 19:04:37 +0100

    graph

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ import { addIcons } from './addIcons.js';
 import { Citysearch } from './citySearch.js';
 import { addResultContainer, addTodayTemperatures, addWeekTemperatures, addOtherCity} from './addDomElements.js';
 import { addCityToLocalStorage } from './storage.js';
+// import { } from './chart.js';
 
 const userInput = document.querySelector('.UserInput');
 
@@ -87,3 +88,22 @@ async function handlePageRefresh() {
     }
 }
 document.addEventListener('DOMContentLoaded', handlePageRefresh);
+
+// ------CHART--------
+const barCanvas = document.getElementById("barCanvas");
+
+const barChart = new Chart(barCanvas, {
+    type: "bar",
+    data: {
+        labels: ["Beijing", "Tokyo", "Seoul", "Hong Kong"],
+        datasets: [
+            {
+                label: "Population",
+                data: [21500000, 13960000, 9720000, 7451000], // Example data (replace with your actual data)
+                backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)"],
+                borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)"],
+                borderWidth: 1
+            }
+        ]
+    }
+});
